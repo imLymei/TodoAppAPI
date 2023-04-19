e7175d6520ac8eeaefb3712c70afd4c8f0e6f8aa
Lymei <lymeicontato@gmail.com>
2023-04-18 21:33:48 -0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express');
 const app = express();
+const modeloTarefa = require('./models/tarefa');
+
 app.use((req, res, next) => {
  res.setHeader("Access-Control-Allow-Origin", "*");
  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
@@ -14,7 +16,97 @@ app.use((req, res, next) => {
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes/routes');
-app.use('/api', routes);
+//app.use('/api', routes);
+
+app.post('/post', async (req, res) => {
+  const objetoTarefa = new modeloTarefa({
+  descricao: req.body.descricao,
+  statusRealizada: req.body.statusRealizada
+  })
+  try {
+      const tarefaSalva = await objetoTarefa.save();
+      res.status(200).json(tarefaSalva)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+ });
+
+app.get('/getAll', async (req, res) => {
+  try {
+      const resultados = await modeloTarefa.find();
+      res.json(resultados)
+  }
+  catch (error) {
+      res.status(500).json({ message: error.message })
+  }
+});
+
+app.delete('/delete/:id', async (req, res) => {
+  try {
+      const id = req.params.id
+      backup.acao = 'DELETE'
+      const resultado = await modeloTarefa.findByIdAndDelete(id)
+      backup.edit[0] = resultado
+      console.log(backup)
+      res.json(resultado)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+ });
+
+app.patch('/undo', async (req,res) =>{
+  try{
+      
+  } catch (error){
+      res.status(400).json({ message: error.message })
+  }
+})
+
+app.patch('/update/:id', async (req, res) => {
+  try {
+      const id = req.params.id;
+      const novaTarefa = req.body;
+      const options = { new: true };
+      const result = await modeloTarefa.findByIdAndUpdate(
+      id, novaTarefa, options
+      )
+      res.json(result)
+  }
+  catch (error) {
+      res.status(400).json({ message: error.message })
+  }
+ });
+
+app.get('/encontrarPorParteDaDescricao/:desc', async (req,res) => {
+  try{
+      const desc = req.params.desc;
+      const result = await modeloTarefa.find({descricao: { $regex: desc}})
+      res.json(result)
+  } catch(error){
+      res.status(400).json({ message: error.message })
+  }
+});
+
+app.delete('/removeAll', async(req,res) =>{
+  try{
+      const result = await modeloTarefa.deleteMany();
+      res.json(result)
+  } catch (error){
+      res.status(400).json({ message: error.message })
+  }
+});
+
+app.delete('/removeAllDone', async(req,res) =>{
+  try{
+      const result = await modeloTarefa.deleteMany({statusRealizada: true});
+      res.json(result)
+  } catch (error){
+      res.status(400).json({ message: error.message })
+  }
+});
+
 app.listen(PORT, () => {
  console.log(`Server Started at ${PORT}`)
 })
@@ -35,3 +127,5 @@ db.on('error', (error) => {
 db.once('connected', () => {
  console.log('Database Connected');
 })
+
+module.exports = app;
